3036dd6fe6a3b227ba21e4409e35930dddbbfd03
mohitkanwaria <mohit.kanwaria.5@gmail.com>
2022-09-22 13:15:55 +0530

    valid

--- src/Controller/userController.js
+++ src/Controller/userController.js
@@ -2,112 +2,130 @@ const userModel = require('../Models/UserModel')
 const validation = require('../validator/validation')
 const jwt = require('jsonwebtoken')
 
-
-
-
 //===============createuser========
-const createUser = async function(req,res){
-try{    
+const createUser = async function (req, res) {
+  try {
     let user = req.body
 
-    if(!validation.isValidRequestBody(user)){
-        return res.status(400).send({
-            status: false,
-            message: "Invalid request parameter, please provide User Details",
-        })
+    if (!validation.isValidRequestBody(user)) {
+      return res.status(400).send({
+        status: false,
+        message: "data is required",
+      })
     }
-    
-    const {title, name, phone, email, password,address} = user
+
+    const { title, name, phone, email, password, address } = user
 
     if (!validation.isValidTitle(title)) {
+      return res
+        .status(400)
+        .send({ status: false, message: "Title is required" });
+    }
+    //============name validation============
+    if (!validation.regixValidator(name)) {
+      return res.status(400).send({
+        status: false,
+        message:
+          "First name is required."
+      });
+    }
+    //===========phone validation=========
+    if (!validation.isValidMobile(phone)) {
+      return res.status(400).send({
+        status: false,
+        message: "Phone no is required."
+      });
+    }
+
+    if (await userModel.findOne({ phone: phone }))
+      return res.status(400).send({ status: false, message: 'Phone number already present!' })
+
+    //======email validation============
+    if (!validation.isValidEmail(email)) {
+      return res.status(400).send({
+        status: false,
+        message: "email is required."
+      });
+    }
+    if (await userModel.findOne({ email: email }))
+      return res.status(400).send({ status: false, message: 'Email Id already present!' })
+
+    //============password validation=======
+    if (!validation.isValidPassword(password)) {
+      return res.status(400).send({
+        status: false,
+        message: "provide valid password"
+      });
+    }
+
+    //==============address validation===========
+
+    if (address) {
+
+      if (!validation.isValid(address.street)) {
         return res
           .status(400)
-          .send({ status: false, message: "Title is required" });
+          .send({ status: false, message: "invalid street" })
       }
 
-      if (!validation.isValid(name)) {
-        return res.status(400).send({
-          status: false,
-          message:
-            "First name is required."
-        });
+      if (!validation.isValid(address.city) || !validation.regixValidator(address.city)) {
+        return res
+          .status(400)
+          .send({ status: false, message: "invalid city" });
       }
 
-      if (!validation.isValidMobile(phone)) {
-        return res.status(400).send({
-          status: false,
-          message: "Phone no is required."
-        });
-      }
-      if(await userModel.findOne({phone:phone}))
-      return res.status(409).send({status:false,message:'Phone number already present!'})
-
-      if (!validation.isValidEmail(email)) {
-        return res.status(400).send({
-          status: false,
-          message: "email is required."
-        });
-      }
-      if(await userModel.findOne({email:email}))
-      return res.status(409).send({status:false,message:'Email Id already present!'})
-
-      if (!validation.isValidPassword(password)) {
-        return res.status(400).send({
-          status: false,
-          message: "provide valid password"
-        });
+      if (! /^\+?([1-9]{1})\)?([0-9]{5})$/.test(address.pincode) || !validation.isValid(address.pincode)) {
+        return res
+          .status(400)
+          .send({ status: false, message: "invalid pincode" })
       }
 
-      if (!validation.isValidPincode(address["pincode"])) {
-        return res.status(400).send({
-          status: false,
-          message: "Pincode no is required."
-        });
-      }
+    }
+
 
     let createNew = await userModel.create(user)
-    res.status(201).send({status:true,message:'Success',data:createNew})
-    }catch(err){
-        res.status(500).send({status:false,message:err.message})
-    }
+    res.status(201).send({ status: true, message: 'Success', data: createNew })
+  } catch (err) {
+    res.status(500).send({ status: false, message: err.message })
+  }
 }
 
 //---------------------- generation of token ----------------------------------- 
-const userLogin = async function(req,res){
-try{    
+const userLogin = async function (req, res) {
+  try {
     let user = req.body
 
-    if(!validation.isValidRequestBody(user)){
-        return res.status(400).send({
-            status: false,
-            message: "Invalid request parameter, please provide User Details",
-        })
+    if (!validation.isValidRequestBody(user)) {
+      return res.status(400).send({
+        status: false,
+        message: "Invalid request parameter, please provide User Details",
+      })
     }
 
     let email = user.email
     let password = user.password
 
     if (!validation.isValidEmail(email)) {
-        return res.status(400).send({
-          status: false,
-          message: "email is required."
-        });
-      }
-      
+      return res.status(400).send({
+        status: false,
+        message: "email is required."
+      });
+    }
 
-      if (!validation.isValidPassword(password)) {
-        return res.status(400).send({
-          status: false,
-          message: "provide valid password"
-        });
-      }
+
+    if (!validation.isValidPassword(password)) {
+      return res.status(400).send({
+        status: false,
+        message: "provide valid password"
+      });
+    }
 
     //validation for userLogin
     let loginUser = await userModel.findOne({ email: email, password: password })
 
     if (!loginUser) {
-    
-        return res.status(400).send({  status: false, msg: "User Details Invalid" })
+
+      return res.status(400).send({ status: false, msg: "User Details Invalid" })
     }
 
     // let currentDate = new Date()
@@ -124,24 +142,24 @@ try{
 
     let token = jwt.sign(
       {
-          loginId:loginUser._id.toString(),
-          userStatus: "active",
-          iat:Date.now()  //issueAt
+        loginId: loginUser._id.toString(),
+        userStatus: "active",
+        iat: Date.now()  //issueAt
       },
       "BookManagementProject3",
-      {expiresIn: "120s"}
-  );
-  let jwtToken={token:token, userId:loginUser._id, iat:Date.now(), exp:new Date(Date.now())}
-    
-      res.setHeader('token-key',token)
+      { expiresIn: "120s" }
+    );
+    let jwtToken = { token: token, userId: loginUser._id, iat: Date.now(), exp: new Date(Date.now()) }
+
+    res.setHeader('token-key', token)
     res.status(200).send({ status: true, message: "Success", data: jwtToken });
-}catch(err){
-    res.status(500).send({status:false, message:err.message})
-}
+  } catch (err) {
+    res.status(500).send({ status: false, message: err.message })
+  }
 }
 
-module.exports.createUser=createUser
-module.exports.userLogin=userLogin
+module.exports.createUser = createUser
+module.exports.userLogin = userLogin
 
 
 
--- src/validator/validation.js
+++ src/validator/validation.js
@@ -1,52 +1,54 @@
 const isValid = function (value) {
-    if (typeof value === "undefined" || value === null) return false;
-    if (typeof value === "string" && value.trim().length > 0) return true;
-    return false;
-  };
+  if (typeof value === "undefined" || value === null) return false;
+  if (typeof value === "string" && value.trim().length > 0) return true;
+  return false;
+};
 
 
-  const isValidTitle = function (title) {
-    return ["Mr", "Mrs", "Miss"].includes(title);
-  };
-  
-  const isValidRequestBody = function (requestBody) {
-    return Object.keys(requestBody).length > 0;
-  };
-  const isValidMobile = function (value){
-    const phone = /^([0|\+[0-9]{1,5})?([7-9][0-9]{9})$/
-    return phone.test(value)
-  }
-  const isValidEmail = function (value) {
-    const regexForEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-    return regexForEmail.test(value)
-  }        
-  
-  const regixValidator = function (value) {
-    let regex = /^[a-zA-Z]+([\s][a-zA-Z]+)*$/ 
-    return regex.test(value)
-  }
-  
-  const isValidPincode = function (pincode) {
-    if ( /^\+?([1-9]{1})\)?([0-9]{5})$/.test(pincode)) return true
+const isValidTitle = function (title) {
+  return ["Mr", "Mrs", "Miss"].includes(title);
+};
+
+const isValidRequestBody = function (requestBody) {
+  return Object.keys(requestBody).length > 0;
+};
+const isValidMobile = function (value) {
+  const phone = /^([0|\+[0-9]{1,5})?([7-9][0-9]{9})$/
+  return phone.test(value)
+}
+const isValidEmail = function (value) {
+  const regexForEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+  return regexForEmail.test(value)
+}
+
+const regixValidator = function (value) {
+  let regex = /^[a-zA-Z]+([\s][a-zA-Z]+)*$/
+  return regex.test(value)
+}
+
+const isValidPincode = function (pincode) {
+  if (/^\+?([1-9]{1})\)?([0-9]{5})$/.test(pincode)) return true
 }
 
 const isValidPassword = function (password) {
-    if (/^[a-zA-Z0-9!@#$%^&*]{8,15}$/.test(password)) return true
+  if (/^[a-zA-Z0-9!@#$%^&*]{8,15}$/.test(password)) return true
 }
 
-let isValidISBN=function(value){
+let isValidISBN = function (value) {
   return /^[6-9]{3}\-([\d]{10})$/.test(value)
 }
+//=================address===========
 
 
-module.exports ={
-    isValid, 
-    isValidTitle,
-    isValidRequestBody,
-    isValidMobile,
-    isValidEmail,
-    regixValidator,
-    isValidPincode,
-    isValidPassword,
-    isValidISBN
+
+module.exports = {
+  isValid,
+  isValidTitle,
+  isValidRequestBody,
+  isValidMobile,
+  isValidEmail,
+  regixValidator,
+  isValidPincode,
+  isValidPassword,
+  isValidISBN
 }
